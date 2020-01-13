8035e4d09170a3e27c02a4442289e13877abc1d8
snekek <nic4096@gmail.com>
2020-01-13 12:34:07 -0800

    oops

--- secret.py
+++
@@ -1,5 +0,0 @@
-google_client_id = '153343989131-ekc53o5ipm3fea7qbqe8s5q4aid2ablu.apps.googleusercontent.com'
-google_client_secret = "Xufdcfw4Qn5DsibISyuEng4q"
-
-github_client_id="f14dd3963f54512958a8",
-github_client_secret="bec6053d51e5b5dd7fc73a4d0e4562687abb8171",
